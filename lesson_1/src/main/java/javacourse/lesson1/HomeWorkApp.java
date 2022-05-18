ad3e22898200a19470d25a6d6ac8ad2cd42a2686
ElenaKorenieva <olena.korenieva@gmail.com>
2022-05-18 22:34:41 +0200

    add files to repository

---
+++ lesson_1/src/main/java/javacourse/lesson1/HomeWorkApp.java
@@ -0,0 +1,51 @@
+package javacourse.lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 5;
+        int b = 2;
+        int sumOfInt = a + b;
+
+        if (sumOfInt >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 9;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int firstNumber = 1;
+        int secondNumber = 4;
+
+        if (firstNumber >= secondNumber) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
